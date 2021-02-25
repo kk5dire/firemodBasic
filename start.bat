48782028576a59155765042b256c66d1ba8b578e
kkd5dire <52354566+kk5dire.commits@users.noreply.github.com>
2021-02-25 06:48:18 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,13 +1,5 @@
 @echo off
 
-echo Installing/updating bot dependencies
-call npm ci --only=production --loglevel=warn >NUL
-
-if NOT ["%errorlevel%"]==["0"] (
-  pause
-  exit /b %errorlevel%
-)
-
 echo Starting the bot
 call npm run start
 
