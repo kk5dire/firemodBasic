5dc65162c87c9107183dd31b0c61b87c066047d7
kkd5dire <52354566+kk5dire.commits@users.noreply.github.com>
2021-02-25 22:32:42 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,3 +3,7 @@ exports.TOKEN = "ODEzNjU1OTg3NTI0NDY4ODA4.YDSeMA.vMvDVo6gAC5BEVuKwsDxf4DbhnE";
 exports.PREFIX = 'mod.';
 
 exports.OWNER_ID = "686039988605026304"
+
+exports.spCID = '3993d774dae64b188e252522dbbe1a8c'
+
+exports.spCS = "44a9fe0b76744a7b89de769d87bd06bb"
