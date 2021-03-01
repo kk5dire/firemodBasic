f4b97d6d803c45c9ee16ad4ec68ec08e3b0c233c
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2021-02-28 21:09:39 -0500

    Create secks.js

---
+++ commands/mod/secks.js
@@ -0,0 +1,12 @@
+module.exports = {
+    config: {
+          name: "secks",
+          description: "ong xdxd funny moment 2021!?!?!?!!?1/1!",
+          aliases: ['seck']
+    },
+  run: async (bot, message, args) => {
+  
+  message.channel.send('secks moment <a:ducke:755797920250593314> <a:ducke:755797920250593314>')
+    
+  },
+};
